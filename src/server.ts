634e18ec581939e7721c437a7043219021c98910
Lucky0108 <raolakshayyadav02@gmail.com>
2023-10-14 02:05:47 +0530

    Changes for webrtc

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import payload from 'payload'
-var cors = require('cors');
+const cors = require('cors');
+const socketIo = require('socket.io');
 
 require('dotenv').config()
 const app = express()
@@ -32,6 +33,28 @@ const start = async () => {
   const server = app.listen(PORT, () => {
 		console.log(`Server is running on port ${PORT}`);
 	});
+
+  const io = socketIo(server, { cors: { origin: '*' } });
+
+  io.on('connection', socket => {
+    console.log('A user connected');
+  
+    // Handle ICE candidates
+    socket.on('iceCandidate', candidate => {
+      console.log(candidate)
+      socket.broadcast.emit('iceCandidate', candidate);
+    });
+  
+    // Handle SDP offer and answer
+    socket.on('sdp', description => {
+      console.log(description)
+      socket.broadcast.emit('sdp', description);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+    });
+  });
 
 }
 
